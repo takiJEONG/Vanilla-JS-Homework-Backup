bcfc80920b99a8eb6da3472afc783f2a16c0942f
takiJEONG <jht3267@gmail.com>
2024-04-27 23:32:54 +0900

    dd
    dd

---
+++ memos.js
@@ -0,0 +1,8 @@
+const h1= document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+ h1.classList.toggle("clicked");
+}
+
+
+h1.addEventListener("click", handleTitleClick);
