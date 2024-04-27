3e675775c4880bd9ac205db87f16188b46f56d36
takiJEONG <jht3267@gmail.com>
2024-04-27 23:35:06 +0900

    ,
    ,

--- memos.js
+++ memos.js
@@ -23,23 +23,6 @@ function resize() {
   }
 }
 window.addEventListener("resize", resize);
-const bd = document.querySelector("body");
-const h1 = document.querySelector("body h1");
-function resize() {
-  let width = window.innerWidth;
-  if (width >= 900) {
-    h1.classList.add("active");
-    bd.style.backgroundColor = "orange";
-  } else if (width < 900 && width >= 800) {
-    h1.classList.add("active");
-    bd.style.backgroundColor = "yellow";
-  } else if (width < 800 && width >= 700) {
-    h1.classList.add("active");
-    bd.style.backgroundColor = "skyblue";
-  }
-}
-window.addEventListener("resize", resize);
 
-//challenge 4월26일 바닐라 JS
 
 //challenge 4월26일 바닐라 JS
